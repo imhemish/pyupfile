32b50701d2481488e50fcff41168526ff1fece6d
Hemish <hemish04082005@gmail.com>
2020-12-07 11:05:14 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+import json
+import clipboard
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+Tk().withdraw()
+filename = askopenfilename()
+myfiles = {'file': open(filename ,'rb')}
+url = "https://file.io"
+postrequest = requests.post(url, files = myfiles)
+postrequestresponse = postrequest.text
+jsonloadedresponse = json.loads(postrequestresponse)
+downloadlink = jsonloadedresponse["link"]
+print(downloadlink)
+print("Download Link Copied to Clipboard. Press Enter to Exit.")
+clipboard.copy(downloadlink)
+exitornot = input()
